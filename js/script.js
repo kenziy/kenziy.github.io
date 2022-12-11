4110dbc76e6a51b8a7bffccd753609d6d0c2e3bf
kenziy <kenneth@vtxsecurity.com>
2022-12-11 14:52:42 +0800

    init

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+  $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav: false,
+        navText: "",
+        dots: false,
+        margin:10,
+        responsiveClass:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        items: 4
+    })
+
+
+});
